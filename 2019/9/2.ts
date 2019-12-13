cd17c6a05353f2660c88502b27fe3f7c423a4d5c
Isaac Riceweber <isaac.riceweber@engrade.com>
2019-12-13 13:47:21 -0800

    2019 day 9 part 2

--- 2019/9/2.ts
+++ 2019/9/2.ts
@@ -7,7 +7,7 @@ import { answer } from '../../shared/answer';
 (async () => {
   const input = await loadInput(join(__dirname, 'input.txt'));
   const program = input[0].split(',').map(i => parseInt(i, 10));
-  const c = new Computer(program, of(1));
+  const c = new Computer(program, of(2));
   const out = [];
   c.output$.subscribe(v => out.push(v));
   try {
