04b6317b795a415b819e47ad9bb15c49923decfa
Isaac Riceweber <brotchen@gmail.com>
2021-12-07 08:50:34 -0800

    feat(2021): Day 7

---
+++ 2021/7/2.ts
@@ -0,0 +1,43 @@
+import { join } from 'path';
+import { answer } from '../../shared/answer';
+import { loadInput } from '../../shared/input';
+
+(async () => {
+  const startingPositions = (await loadInput(join(__dirname, 'input.txt')))[0]
+    // const startingPositions = '16,1,2,0,4,2,7,1,2,14'
+    .split(',')
+    .map((n) => parseInt(n, 10));
+  const mean =
+    startingPositions.reduce((t, n) => (t = n), 0) / startingPositions.length;
+  let target = mean % 1 > 0.5 ? Math.ceil(mean) : Math.floor(mean);
+
+  spentFuel(startingPositions, 2);
+
+  let targetFuel = spentFuel(startingPositions, target);
+  let leftFuel = spentFuel(startingPositions, target - 1);
+  let rightFuel = spentFuel(startingPositions, target + 1);
+
+  while (leftFuel < targetFuel || rightFuel < targetFuel) {
+    if (leftFuel < rightFuel) {
+      target -= 1;
+      rightFuel = targetFuel;
+      targetFuel = leftFuel;
+      leftFuel = spentFuel(startingPositions, target - 1);
+    } else {
+      target += 1;
+      leftFuel = targetFuel;
+      targetFuel = rightFuel;
+      rightFuel = spentFuel(startingPositions, target + 1);
+    }
+  }
+
+  console.log(target);
+  answer(targetFuel);
+})().catch(console.error);
+
+function spentFuel(startingPositions: number[], target: number): number {
+  return startingPositions.reduce((total, position) => {
+    const diff = Math.abs(position - target);
+    return total + Math.floor(0.5 * diff * (diff + 1));
+  }, 0);
+}
